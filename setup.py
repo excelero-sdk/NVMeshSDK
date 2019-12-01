f6b9e42e38dd9a073618c3da04b2deb46cfd1bec
Ron Petrov <ron.petrov@excelero.com>
2019-12-01 16:25:45 +0200

    New tag: 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='NVMeshSDK',
-    version='0.1.2',
+    version="0.1.3",
     author='Excelero, Inc.',
     url='https://github.com/excelero-sdk/NVMeshSDK',
     license='MIT', 
